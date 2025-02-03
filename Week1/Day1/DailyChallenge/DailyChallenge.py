ef7b3c7d5e0b2b231d50f8eb34a5d724c5fefe8b
Ohzdt1 <ohzdt1@gmail.com>
2025-02-03 09:49:05 +0200

    Clean up DailyChallange

--- Week1/Day1/DailyChallenge/DailyChallenge.py
+++ Week1/Day1/DailyChallenge/DailyChallenge.py
@@ -1,4 +1,7 @@
-import random
+
+#1 Using the input function, ask the user for a string. The string must be 10 characters long.
+#2 Then, print the first and last characters of the given text.
+
 
 user_input = input('Please write a 10 character string :\n')
 if len(user_input) < 10 :
@@ -11,9 +14,15 @@ print(user_input[0])
 print(user_input[9])
 
 
+#3. Using a for loop, construct the string character by character: Print the first character, then the second, then the third, until the full string is printed. For example:
+
 for i in range(len(user_input)):
         print(user_input[:i+1])
 
+
+#4. Bonus: Swap some characters around then print the newly jumbled string (hint: look into the shuffle method). For example:
+
+import random
 
 char_list = list(user_input)
 random.shuffle(char_list) 

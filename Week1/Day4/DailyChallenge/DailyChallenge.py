7a73de2ee647a9dbfc5b75990242c14519eb9697
Ohzdt1 <ohzdt1@gmail.com>
2025-02-06 09:59:53 +0200

    Daily challange day4

--- Week1/Day4/DailyChallenge/DailyChallenge.py
+++ Week1/Day4/DailyChallenge/DailyChallenge.py
@@ -1,9 +1,35 @@
-MATRIX_STRING = '''7ii
-Tsx
-h%?
-i #
-sM 
-$a 
-#t%
-^r!'''
+#used external tools for this as I did not manage by myself 
 
+matrix = [
+    ['7', 'i', 'i'],
+    ['T', 's', 'x'],
+    ['h', '%', '?'],
+    ['i', ' ', '#'],
+    ['s', 'M', ' '],
+    ['$', 'a', ' '],
+    ['#', 't', '%'],
+    ['^', 'r', '!']
+]
+
+
+def decode_matrix(matrix):
+    rows = len(matrix)
+    cols = len(matrix[0])
+
+    decoded_text = ""
+    temp_text = ""
+
+    for col in range(cols):
+        for row in range(rows):
+            char = matrix[row][col]
+            if char.isalpha():  
+                temp_text += char
+            else:  
+                temp_text += " "
+
+    decoded_text = ' '.join(temp_text.split())
+
+    return decoded_text
+
+message = decode_matrix(matrix)
+print("Decoded Message:", message)
